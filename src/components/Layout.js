64c9669212808dce1ac98361bdb4b648eeaf8725
najaram <arambulo.jan@gmail.com>
2020-03-03 11:43:34 +0800

    test metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,31 +9,74 @@ import Footer from './Footer';
 export default class Body extends React.Component {
     render() {
         return (
-            <React.Fragment>
-                <Helmet>
-                    <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
-                    <meta charSet="utf-8"/>
-                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
-                    <meta name="google" content="notranslate" />
-                    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i" rel="stylesheet"/>
-                    <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
-                    {(_.get(this.props, 'pageContext.frontmatter.template') === 'post') &&  
-                    _.get(this.props, 'pageContext.frontmatter.canonical_url') && 
-                    <link rel="canonical" href={_.get(this.props, 'pageContext.frontmatter.canonical_url')}/>
-                    }
-                </Helmet>
-                <div id="page" className={'site style-' + _.get(this.props, 'pageContext.site.siteMetadata.layout_style') + ' palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
-                  <Header {...this.props} />
-                  <div id="content" className="site-content">
-                    <div className="inner">
-                      <main id="main" className="site-main">
-                        {this.props.children}
-                      </main>
-                      <Footer {...this.props} />
-                    </div>
-                  </div>
+          <React.Fragment>
+            <Helmet>
+              <title>
+                {_.get(this.props, "pageContext.frontmatter.title") &&
+                  _.get(this.props, "pageContext.frontmatter.title") + " - "}
+                {_.get(this.props, "pageContext.site.siteMetadata.title")}
+              </title>
+              <meta charSet="utf-8" />
+              <meta
+                name="viewport"
+                content="width=device-width, initialScale=1.0"
+              />
+              <meta name="google" content="notranslate" />
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta
+                name="twitter:image"
+                content={_.get(
+                  this.props,
+                  "pageContext.site.siteMetadata.image"
+                )}
+              />
+              <meta
+                name="twitter:creator"
+                content={_.get(
+                  this.props,
+                  "pageContext.site.siteMetadata.twitterUsername"
+                )}
+              />
+              <link
+                href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i"
+                rel="stylesheet"
+              />
+              <link rel="stylesheet" href={safePrefix("assets/css/main.css")} />
+              {_.get(this.props, "pageContext.frontmatter.template") ===
+                "post" &&
+                _.get(this.props, "pageContext.frontmatter.canonical_url") && (
+                  <link
+                    rel="canonical"
+                    href={_.get(
+                      this.props,
+                      "pageContext.frontmatter.canonical_url"
+                    )}
+                  />
+                )}
+            </Helmet>
+            <div
+              id="page"
+              className={
+                "site style-" +
+                _.get(
+                  this.props,
+                  "pageContext.site.siteMetadata.layout_style"
+                ) +
+                " palette-" +
+                _.get(this.props, "pageContext.site.siteMetadata.palette")
+              }
+            >
+              <Header {...this.props} />
+              <div id="content" className="site-content">
+                <div className="inner">
+                  <main id="main" className="site-main">
+                    {this.props.children}
+                  </main>
+                  <Footer {...this.props} />
                 </div>
-            </React.Fragment>
+              </div>
+            </div>
+          </React.Fragment>
         );
     }
 
