e50b3caf262c3c30fe5d57c7dc06f5ee0cf360c1
najaram <arambulo.jan@gmail.com>
2020-04-06 19:31:23 +0800

    adddddsssssss

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,17 +7,31 @@
 const React = require("react");
 const safePrefix = require("./src/utils/safePrefix").default;
 
-exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+exports.onRenderBody = function ({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        
+        <script dangerouslySetInnerHTML={{
+            __html:`window._mNHandle = window._mNHandle || {};
+            window._mNHandle.queue = window._mNHandle.queue || [];
+            medianet_versionId = "3121199";`
+        }} />,
+        <script src="https://contextual.media.net/dmedianet.js?cid=8CUH6F904" async="async"></script>
     ]);
 
     setPostBodyComponents([
         <React.Fragment>
-            <script src={safePrefix('assets/js/plugins.js')}/>
-            <script src={safePrefix('assets/js/main.js')}/>
-            
+            <script src={safePrefix('assets/js/plugins.js')} />
+            <script src={safePrefix('assets/js/main.js')} />
+            <div id="886859209">
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    try {
+                        window._mNHandle.queue.push(function () {
+                            window._mNDetails.loadTag("886859209", "728x90", "886859209");
+                        });
+                    } catch (error) {}`
+                }} />
+            </div>
         </React.Fragment>
     ]);
 
